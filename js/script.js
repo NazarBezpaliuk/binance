c82d6a1b1e37351cc00299a78c49835b4e22b5aa
NazarBezpaliuk <nazardxvx@gmail.com>
2022-10-17 16:31:16 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// document.querySelector(".menu_btn").addEventListener("click", (event) => {
+//   event.preventDefault();
+//   if (localStorage.getItem("theme") === "dark") {
+//     localStorage.removeItem("theme");
+//   } else {
+//     localStorage.setItem("theme", "dark");
+//   }
+//   addDarkClassToHTML();
+// });
+
+// function addDarkClassToHTML() {
+//   try {
+//     if (localStorage.getItem("theme") === "dark") {
+//       document.querySelector(".menu").classList.add("menu__open");
+//     } else {
+//       document.querySelector(".menu").classList.remove("menu__open");
+//     }
+//   } catch (err) {}
+// }
+
+// addDarkClassToHTML();
